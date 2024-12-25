408ae35427168dbb64021f3b5eea30163bf5e680
Sk7Str1pe <finenkofedor@gmail.com>
2024-12-25 20:17:35 +0300

    upd

---
+++ nix/manager.nix
@@ -0,0 +1,22 @@
+{pkgs, ...}: {
+  nix = {
+    enable = true;
+    package = pkgs.nixVersions.latest;
+    settings = {
+      use-sandbox = true;
+      show-trace = true;
+      allowed-users = [
+        "*"
+      ];
+      auto-optimise-store = true;
+      cores = 8;
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      sandbox-paths = ["/bin/sh=${pkgs.busybox-sandbox-shell.out}/bin/busybox"];
+    };
+    optimise.automatic = true;
+    gc.automatic = true;
+  };
+}
