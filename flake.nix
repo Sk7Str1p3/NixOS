cf9bd39f18a2ab11ff072129b7ff6d2a8cc4c91a
Sk7Str1pe <finenkofedor@gmail.com>
2025-01-26 11:59:46 +0300

    flake.nix => set lanzaboote release

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     agenix.url = "github:ryantm/agenix";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
