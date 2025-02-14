d0e96c53009248549c2bebcdf899956eca401d8d
Sk7Str1pe <finenkofedor@gmail.com>
2025-02-14 20:47:28 +0300

    fix: use whitelist

--- network/zapret.nix
+++ network/zapret.nix
@@ -5,13 +5,26 @@
       "--dpi-desync=syndata,fake,split2 --dpi-desync-fooling=md5sig"
       "--dpi-desync=fake,tamper --dpi-desync-any-protocol"
     ];
-    blacklist = [
-      "gosuslugi.ru"
-      "russmo.getcourse.ru"
-      "rutube.ru"
-      "reddit.com"
-      "nixos.org"
-      "habr.com"
+    whitelist = [
+      "discord.com"
+      "gateway.discord.gg"
+      "cdn.discordapp.com"
+      "discordapp.net"
+      "discordapp.com"
+      "discord.gg"
+      "media.discordapp.net"
+      "images-ext-1.discordapp.net"
+      "discord.app"
+      "discord.media"
+      "discordcdn.com"
+      "discord.dev"
+      "discord.new"
+      "discord.gift"
+      "discordstatus.com"
+      "dis.gd"
+      "discord.co"
+      "discord-attachments-uploads-prd.storage.googleapis.com"
+      "cloudflare-ech.com"
     ];
   };
 }
